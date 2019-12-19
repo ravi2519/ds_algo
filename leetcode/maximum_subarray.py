dc2351eaff5a25c61b5c828418cf59e2ca5f44b9
Ravi Ranjan <ravi2519@gmail.com>
2019-12-19 11:52:28 +0000

    Initial commit

---
+++ leetcode/maximum_subarray.py
@@ -0,0 +1,26 @@
+'''
+https://leetcode.com/problems/maximum-subarray/
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+'''
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        best = nums[0]
+        cur = 0
+        
+        for num in nums:
+            cur += num
+            if cur > best:
+                best = cur
+            if cur < 0:
+                cur = 0
+        
+        return best
---
+++ main.py
@@ -0,0 +1,8 @@
+from leetcode import rotate_array
+
+nums = [1,2,3,4,5,6,7]
+k = 3
+
+s = rotate_array.Solution()
+s.rotate(nums, k )
+print(nums)
